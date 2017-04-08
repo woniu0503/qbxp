45ca80ff77bbedba1b1ee31509630ff5c65d04bb
woniu0503 <wodegithub0503>
2017-04-08 21:02:01 +0800

    11

---
+++ cggcsWeb/js/tagVal.js
@@ -0,0 +1,4 @@
+// JavaScript Document
+if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ){
+window.location.href = "tagValError.html";  //可以换成http地址
+}
